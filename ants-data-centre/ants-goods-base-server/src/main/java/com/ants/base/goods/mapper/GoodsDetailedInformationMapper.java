80f8c76f4716df5a57089d6156a261816d70f297
LiuZhi <904412862@qq.com>
2020-11-30 18:44:02 +0800

    1.修改商品品牌 商品基本数据 类名 Good***——>Goods***

--- ants-data-centre/ants-goods-base-server/src/main/java/com/ants/base/goods/mapper/GoodsDetailedInformationMapper.java
+++ ants-data-centre/ants-goods-base-server/src/main/java/com/ants/base/goods/mapper/GoodsDetailedInformationMapper.java
@@ -8,6 +8,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author
  * @date 2020-05-25
  */
-public interface GoodDetailedInformationMapper extends BaseMapper<GoodsDetailedInformation> {
+public interface GoodsDetailedInformationMapper extends BaseMapper<GoodsDetailedInformation> {
 
 }
