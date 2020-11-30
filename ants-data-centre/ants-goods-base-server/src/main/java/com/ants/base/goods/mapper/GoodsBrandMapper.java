45cae10d71ec1e62483335e400e3e0a258fcdee6
LiuZhi <904412862@qq.com>
2020-11-30 18:37:19 +0800

    1.修改商品品牌 商品基本数据 类名 Good***——>Goods***

--- ants-data-centre/ants-goods-base-server/src/main/java/com/ants/base/goods/mapper/GoodsBrandMapper.java
+++ ants-data-centre/ants-goods-base-server/src/main/java/com/ants/base/goods/mapper/GoodsBrandMapper.java
@@ -3,5 +3,5 @@ package com.ants.base.goods.mapper;
 import com.ants.base.goods.entity.GoodBrand;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
-public interface GoodBrandMapper extends BaseMapper<GoodBrand> {
+public interface GoodsBrandMapper extends BaseMapper<GoodBrand> {
 }
